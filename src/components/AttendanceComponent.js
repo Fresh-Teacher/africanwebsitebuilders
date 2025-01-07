670fdd56c8f61221d9bf1a91fbbef44790d58eb3
Fresh-Teacher <98003374+Fresh-Teacher@users.noreply.github.com>
2025-01-07 08:11:32 +0200

    Update AttendanceComponent.js

--- src/components/AttendanceComponent.js
+++ src/components/AttendanceComponent.js
@@ -620,7 +620,7 @@ const GroupedAttendanceComponent = ({ students }) => {
                   onClick={handleClearAttendance}
                   className="w-full sm:w-auto px-3 py-2 bg-red-100 text-red-600 dark:bg-red-900 dark:text-red-300 rounded hover:bg-red-200 dark:hover:bg-red-800 transition-colors"
                 >
-                  Clear All
+                  Clear Attendance Data 🚮
                 </button>
               </div>
             </div>
